75a0409cba366d1895749c1d4e8bd185e3dc0980
Chetan kumar <chetanhoney1993@gmail.com>
2022-08-02 22:22:48 +0530

    Initial project setup

---
+++ CartrackAssignment/CartrackAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartrackAssignmentApp.swift
+//  CartrackAssignment
+//
+//  Created by chetan kumar on 01/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CartrackAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
